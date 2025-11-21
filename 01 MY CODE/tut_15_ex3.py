8175f76c9d0f0204b9ddb968eab10d024db85823
Himeshkar11 <himeshkarm0111@gmail.com>
2025-11-21 18:49:35 +0530

    revised till functions today

--- 01 MY CODE/tut_15_ex3.py
+++ 01 MY CODE/tut_15_ex3.py
@@ -1,22 +1,24 @@
 n =18
-chansuu = 9
+
+# a guess the game program 
+chance = 9
 print("Welcome to the game \n you have 9 choices to guess the number \n after that game over")
-while chansuu >0:
-    lelo = int(input("Enter your number here "))
-    if lelo == n :
-        print("Hai sekai desu ")
+while chance >0:
+    user_input = int(input("Enter your number here "))
+    if user_input == n :
+        print(f"Hai number you chose{user_input} is correct  ")
         break
-    elif lelo >n :
-        print("The number you have enterd is bigger then the number ")
-        chansuu -= 1
-        print("You have", chansuu, "chances left.\n")
+    elif user_input >n :
+        print("The number you have enter is bigger then the number ")
+        chance -= 1
+        print(f"You have{chance} chances left.\n")
 
     else :
 
         print("The number you have entered is smaller than the number")
-        chansuu -= 1
-        print("You have", chansuu, "chances left.\n")
+        chance -= 1
+        print(f"You have{chance} chances left.\n")
 
-    if chansuu == 0:
-        print("Game over! The correct number was", n)
+    if chance == 0:
+        print(f"Game over! The correct number was {n}")
     print("Thanks for playing the game !")
